9257d6ea81a41ec593c3ce391ffd347fadf00707
brand_mg86@student.itstep.org <brand_mg86@student.itstep.org>
2026-09-03 20:30:16 +0200

    HW 03.09.2026

--- 02_Operators/02_Operators.cpp
+++ 02_Operators/02_Operators.cpp
@@ -359,59 +359,252 @@ int main()
 	//}
 
 
-	cout << "\n 4 ";
-	cout << "\n  ";
-	int animal;
-	cout << "1 - Zebra" << endl;
-	cout << "2 - Dog " << endl;
-	cout << "3 - Lion" << endl;
-	cout << "4 - Elephant" << endl;
-	cout << "5 - Wolf" << endl;
-	cout << "6 - Tiger" << endl;
-	cout << "7 - Giraffe" << endl;
+	//cout << "\n 4 ";
+	//cout << "\n  ";
+	//int animal;
+	//cout << "1 - Zebra" << endl;
+	//cout << "2 - Dog " << endl;
+	//cout << "3 - Lion" << endl;
+	//cout << "4 - Elephant" << endl;
+	//cout << "5 - Wolf" << endl;
+	//cout << "6 - Tiger" << endl;
+	//cout << "7 - Giraffe" << endl;
 
-	cout << " Select an animal: ";
-	cin >> animal;
+	//cout << " Select an animal: ";
+	//cin >> animal;
 
-	if (animal == 1 || animal == 4 || animal == 7) {
-		cout << "herbivore";
-	}
-	else if (animal == 2 || animal == 5 || animal == 6) {
-		cout << "predator";
-	}
-	else {
-		cout << "Invalid number!";
-	}
+	//if (animal == 1 || animal == 4 || animal == 7) {
+	//	cout << "herbivore";
+	//}
+	//else if (animal == 2 || animal == 5 || animal == 6) {
+	//	cout << "predator";
+	//}
+	//else {
+	//	cout << "Invalid number!";
+	//}
 
 	//cout << "\n  ";
 
-	switch (animal) {
-		case 1:
-			cout << "herbivore";
-			break;
-		case 2:
-			cout << "predator";
-			break;
-		case 3:
-			cout << "predator";
-			break;
-		case 4:
-			cout << "herbivore";
-			break;
-		case 5:
-			cout << "predator";
-			break;
-		case 6:
-			cout << "predator";
-			break;
-		case 7:
-			cout << "herbivore";
-			break;
-		default:
-			cout << "Invalid number!";
+	//switch (animal) {
+	//	case 1:
+	//		cout << "herbivore";
+	//		break;
+	//	case 2:
+	//		cout << "predator";
+	//		break;
+	//	case 3:
+	//		cout << "predator";
+	//		break;
+	//	case 4:
+	//		cout << "herbivore";
+	//		break;
+	//	case 5:
+	//		cout << "predator";
+	//		break;
+	//	case 6:
+	//		cout << "predator";
+	//		break;
+	//	case 7:
+	//		cout << "herbivore";
+	//		break;
+	//	default:
+	//		cout << "Invalid number!";
+	//}
+
+
+	//cout << "\n  Home Work";
+	//cout << "\n 1 ";
+	//cout << "\n  ";
+
+	//double a;
+	//double b;
+	//cout << "Enter side a : "; cin >> a;
+	//cout << "Enter side b : "; cin >> b;
+	//if (a == b) {
+	//	cout << " The rectangle is a square. " << endl;
+	//}
+	//else {
+	//	cout << " The rectangle is not a square. " << endl;
+	//}
+
+
+	/*cout << "\n 2 ";
+	cout << "\n  ";
+
+	int age;
+	cout << " Enter age: "; cin >> age;
+
+
+	if (age < 0) {
+		cout << "Invalid input" << endl;
 	}
 
+	else if (age < 1) {
+		cout << "The child is still a baby and is allowed to stay at home with their parents." << endl;
+	}
+	else if (age >= 1 && age <= 5) {
+		cout << "The child is allowed to go to kindergarten." << endl;
+	}
+	else if (age >= 6 && age <= 18) {
+		cout << "The child has to go to school." << endl;
+	}
+	else if (age >= 19) {
 
+	}
+	else {
+		cout << "The person is an adult and is allowed to study or work." << endl;
+	}*/
+
+	//cout << "\n 3 ";
+	//cout << "\n  ";
+
+	//int a, b, c;
+	//cout << "Enter three numbers: " << endl;
+	//cin >> a >> b >> c;
+
+	//if ((a < 0 && b < 0) || (a < 0 && c < 0) || (c < 0 && b < 0)) {
+	//	cout << " Exactly two number are negative." << endl;
+	//}
+	//else {
+	//	cout << " Exactly two number are not negative." << endl;
+	//}
+
+	//cout << "\n 4 ";
+	//cout << "\n  ";
+
+
+	//double d;
+	//cout << "Enter a number: " << endl;
+	//cin >> d;
+
+	//if (d >= -1 && d <= 1) {
+	//	cout << " The number belongs to the interval [-1, 1]." << endl;
+	//}
+	//else if (d >= 2 && d <= 5) {
+	//	cout << " The number belongs to the interval [2, 5]." << endl;
+	//}
+	//else {
+	//	cout << " The number does not belong to the intervals [-1, 1] and [2, 5]." << endl;
+	//}
+
+
+	//cout << "\n 5 ";
+	//cout << "\n  ";
+
+
+	//int a, b, c;
+	//cout << "Enter three numbers: " << endl;
+	//cin >> a >> b >> c;
+
+	//if ((a == b && a != c) || (a == c && a != b) || (c == b && a != b)) {
+	//	cout << " Exactly two number are equal." << endl;
+	//}
+	//else {
+	//	cout << " Exactly two number are not equal." << endl;
+	//}
+
+	//cout << "\n 6 ";
+	//cout << "\n  ";
+
+
+	//int a, b, c;
+	//cout << "Enter three numbers: " << endl;
+	//cin >> a >> b >> c;
+
+	//if (a % 2 != 0 && b % 2 != 0 && c % 2 != 0) {
+	//	cout << " All three numbers are odd." << endl;
+	//}
+	//else {
+	//	cout << " Not all three numbers are odd." << endl;
+	//}
+
+
+	/*cout << "\n 7 ";
+	cout << "\n  ";
+
+	int a, b;
+	cout << "Enter two numbers: " << endl;
+	cin >> a >> b;
+
+	if (a != b) {
+		int x = a;
+		a = b;
+		b = x;
+	}
+	cout << "a = " << a << endl;
+	cout << "b = " << b << endl;*/
+
+	//cout << "\n 8 ";
+	//cout << "\n  ";
+
+	//int a;
+	//cout << "Enter a three-digit number: " << endl;	cin >> a;
+
+	//int first = a / 100;
+	//int middle = a / 10 % 10;
+	//int last = a % 10;
+
+	//int sum = first + middle + last;
+
+	//cout << "Number of digits: 3" << endl;
+	//cout << "Sum of digits: " << sum << endl;
+	//cout << "First digit: " << first << "  " << "Last digit: " << last << endl;
+
+	//cout << "\n 9 ";
+	//cout << "\n  ";
+
+	//int hours, minutes, seconds;
+
+	//cout << " Enter hours: "; cin >> hours;
+	//cout << " Enter minutes: "; cin >> minutes;
+	//cout << " Enter seconds: "; cin >> seconds;
+
+	//if ((hours >= 0 && hours <= 23) && (minutes >= 0 && minutes <= 59) && (seconds >= 0 && seconds <= 59)) {
+	//	cout << " The time is valid." << endl;
+	//}
+	//else {
+	//	cout << " The time is not valid." << endl;
+	//}
+
+	/*cout << "\n 10 ";
+	cout << "\n  ";
+
+	int hour;
+
+	cout << " Enter hour: "; cin >> hour;
+
+	if (hour >= 0 && hour <= 5) {
+		cout << "Good night" << endl;
+	}
+	else if (hour >= 6 && hour <= 11) {
+		cout << "Good mornig" << endl;
+	}
+	else if (hour >= 12 && hour <= 17) {
+		cout << "Good day" << endl;
+	}
+	else if (hour >= 18 && hour <= 23) {
+		cout << "Good evening" << endl;
+	}
+	else {
+		cout << " Invalid hour" << endl;
+	}*/
+
+	cout << "\n 11 ";
+	cout << "\n  ";
+
+	int a, b, c;
+	cout << "Enter three numbers: " << endl;
+	cin >> a >> b >> c;
+
+	if (a <= b && a <= c) {
+		cout << "Minimum:" << a << endl;
+	}
+	else if (b <= a && b <= c) {
+		cout << "Minimum:" << b << endl;
+	}
+	else {
+		cout << "Minimum:" << c << endl;
+	}
 
 
 
