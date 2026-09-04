eee5f936041330a4e512042b03fb46bbfab43922
brand_mg86@student.itstep.org <brand_mg86@student.itstep.org>
2026-09-04 20:32:30 +0200

    Home work 04.09.2026

--- 04_EnumCycles_04_09_26/04_EnumCycles_04_09_26.cpp
+++ 04_EnumCycles_04_09_26/04_EnumCycles_04_09_26.cpp
@@ -14,6 +14,7 @@ int main()
 	//	cout << i << endl;
 	//	i++;
 	//}
+	
 	// cout << "\n 2 ";
    // cout << "\n  ";
 
@@ -69,7 +70,7 @@ int main()
 	//cout << "\n 5 ";
 	//cout << "\n  ";
 
-	int number;
+	/*int number;
 	int sum = 0 ;
 
 	for ( ; ; )
@@ -84,7 +85,121 @@ int main()
 		sum += number;
 	}
 
-	cout << "Sum: " << sum << endl;
+	cout << "Sum: " << sum << endl;*/
+
+	
+
+
+
+
+
+	//cout << Home work"\n ";
+	//cout << "\n 1.Вивести на екран цілі числа від 14 до 123. - while ";
+
+	//int i = 14;
+	//while (i <= 123)
+	//{
+	//	cout << i << endl;
+	//	i++;
+	//}
+
+	/* cout << "\n 2.Вивести на екран додатні непарні числа, які не перевищують 100. - while ";*/
+
+	//int i = 1;
+	//int count = 0;
+
+	//while (i <= 100)
+	//{
+	//	if (i % 2 != 0)
+	//		{
+	//		cout << i << " " << endl;
+	//		count++;
+	//		}
+	//	i += 1;
+	//}
+
+	/*cout << 3.Дано N (N>0). Вводяться N чисел. Знайти кількість від’ємних серед них. - while ";*/
+
+	//int n;
+	//int i = 0;
+	//int number;
+	//int count = 0;
+
+	//cout << "Enter how many numbers you want to enter: ";
+	//cin >> n;
+
+	//while (i < n)
+	//{
+	//	cout << "Enter number: ";
+	//	cin >> number;
+
+	//	if (number < 0)
+	//	{
+	//		count += 1;
+	//	}
+	//	i++;
+
+	//}
+	//cout << "Number of negative numbers: " << count << endl;
+
+	/*cout << "\n 4.Вводяться 8 чисел. Знайти добуток та середнє арифметичне цих чисел. - while ";*/
+
+	//
+	//int i = 0;
+	//int number;
+	//int sum = 0;
+	//int product = 1;
+
+
+	//while (i < 8)
+	//{
+	//	cout << "Enter eight number: ";
+	//	cin >> number;
+
+	//	sum += number;
+	//	product *= number;
+	//	i++;
+
+	//}
+	//cout << "Product: " << product << endl;
+	//cout << "Average: " << (double) sum/8 << endl;
+
+	
+	//cout << "\n 5.Вивести на екран цілі непарні  числа : 100 98 96 94...4  2 1. - do..while ";
+
+
+	//int i = 99;
+	//
+	//do
+	//{
+	//	cout << i << " " << endl;
+	//	i -= 2;
+	//} 
+	//while (i >=1 );
+
+
+
+	//cout << "6.Написати програму яка обчислює добуток п’яти чисел, які вводяться з клавіатури. - do..while";
+
+	int i = 0;
+	int number;
+	int product = 1;
+
+	do
+	{
+		cout << "Enter number " << endl; cin >> number;
+		product *= number;
+		i++;
+	} 
+	while (i < 5 );
+	cout << "Product: " << product << endl;
+
+
+
+
+
+
+
 
 
 
