4917c460b61174a2460711b40319b6331ea2f054
brand_mg86@student.itstep.org <brand_mg86@student.itstep.org>
2026-09-18 20:20:25 +0200

    Exercice 1

--- 10_Practical_work/10_Practical_work.cpp
+++ 10_Practical_work/10_Practical_work.cpp
@@ -3,7 +3,6 @@
 #include <iostream>
 using namespace std;
 
-
 void InitArray(int arr[], int size)
 {
 	for (int i = 0; i < size; i++)
@@ -11,6 +10,30 @@ void InitArray(int arr[], int size)
 		arr[i] = rand() % 100;
 	}
 }
+
+void SortArray(int arr[], int size, int direction =1 )
+{
+	for (int i = 0; i < size -1; i++)
+	{
+		for (int j = 0; j < size-j-1; j++) 
+		{	
+			if (direction == 1 && arr[j] < arr[j+1]) 
+			{
+				int temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j+1] = temp;
+			}
+			if(direction == 0 && arr[j] > arr[j + 1])
+			{
+				int temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+		}
+	}
+}
+
+
 void ShowArray(int arr[], int size)
 {
 	for (int i = 0; i < size; i++)
@@ -19,40 +42,6 @@ void ShowArray(int arr[], int size)
 	}
 	cout << endl;
 }
-void BubbleSort(int arr[], int size, int direction =1 )
-{
-	for (int i = 0; i < size -1; i++)
-	{
-		for (int j = 0; j < size-j-1; j++) {	
-				if (direction == 1 && arr[j] < arr[j+1]) 
-				{
-					int temp = arr[j];
-					arr[j] = arr[j + 1];
-					arr[j+1] = temp;
-				}
-				if(direction == 0 && arr[j] < arr[j + 1])
-				{
-					int temp = arr[j];
-					arr[j] = arr[j + 1];
-					arr[j + 1] = temp;
-				}
-		}
-	}
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -61,7 +50,24 @@ void BubbleSort(int arr[], int size, int direction =1 )
 
 int main()
 {
-   
+	srand(time(0));
+
+	{
+		int arr[10];
+
+		InitArray(arr, 10);
+		cout << "Array: " << endl;
+		ShowArray(arr, 10);
+
+		SortArray(arr, 10, 0);
+		cout << "Ascending order: " << endl; //Aufsteigend:
+		ShowArray(arr, 10);
+
+		SortArray(arr, 10, 1);
+		cout << "Descending order: " << endl; //Absteigend:
+		ShowArray(arr, 10);
+	}
+
 
 
 
